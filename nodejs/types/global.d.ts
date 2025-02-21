1df29540547f40ef4943f5ea55f947b5ce179438
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2025-02-22 01:45:42 +0530

    fixed type issues

---
+++ nodejs/types/global.d.ts
@@ -0,0 +1,45 @@
+declare global {
+    interface User {
+        id: string;
+        name: string;
+        logo: string;
+        rooms: Array<Room>;
+    }
+    var User: User;
+
+    interface AI {
+        id: string;
+        name: string;
+        logo: string;
+    }
+    var AI: AI;
+
+    interface Room {
+        id: string;
+        name: string;
+        members: Array<{ user: User; access: RoomAccess }>;
+        chat_history: Message[];
+    }
+    var Room: Room;
+    enum RoomAccess {
+        READ = 0,
+        WRITE = 1,
+        ADMIN = 2,
+    }
+
+    type Message = {
+        id: string;
+        sender: string; // AI OR USER id
+        senderType: MessageSender;
+        content: string;
+        createdAt: Date;
+    };
+    var Message: Message;
+    enum MessageSender {
+        USER = 0,
+        AI = 1,
+        ADMIN = 2,
+    }
+}
+
+export {};
