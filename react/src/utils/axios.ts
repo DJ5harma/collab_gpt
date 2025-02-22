74ca732da9684f1cb1fa0155f37fd0d17fc324e4
Dhananjay Bhargav Sharma <138970319+DJ5harma@users.noreply.github.com>
2025-02-22 19:55:00 +0530

    reset react

--- react/src/utils/axios.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const auth_api = axios.create({
-    baseURL: "http://localhost:4000/auth",
-    withCredentials: true,
-});
--- react/src/utils/do_every_n_days.ts
+++
@@ -1,6 +0,0 @@
-export function do_every_n_days(n: number) {
-    const now = Date.now();
-    const daysSinceEpoch = Math.floor(now / (1000 * 60 * 60 * 24)); // Convert to days
-    const shouldTrigger = daysSinceEpoch % n === 0; // Every 2 days
-    return shouldTrigger;
-}
